2f8ef471812e17a06eae83d306c1f3fb4d1a5f70
GOSSET RAPHAEL p2216446 <raphael.gosset@etu.univ-lyon1.fr>
2024-02-15 15:41:20 +0100

    ajout masque à la fonction SDLSDL_CreateRGBSurfaceFrom, ce qui n'a résolu que en parti les couleurs bizarre

--- P2210198_P2211174_P2216446/src/mainAffichage.cpp
+++ P2210198_P2211174_P2216446/src/mainAffichage.cpp
@@ -4,7 +4,7 @@
 
 int main (int argc, char** argv) {
  Image image (1000,1000);
- image.ouvrir("./data/image1.ppm");
+ image.ouvrir("./data/image3.ppm");
 
  ImageViewer imview;
  imview.afficher(image);
